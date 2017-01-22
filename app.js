f5a4ff1863109c138b0ad47bae7124f44dd11e74
Imre Nagi <imre.nagi2812@gmail.com>
2017-01-22 09:52:03 -0800

    Add some configs

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.set("view options", {layout: false});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./routes'))
